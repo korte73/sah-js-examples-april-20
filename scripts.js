3b3f9c922237b876c4518f23c9dbb69f75e1be92
Adam Gyulavari <adam.gyulavari@greenfoxacademy.com>
2020-04-30 17:30:43 +0200

    add jquery first few examples

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+// Third part:
+
+$('h1').html('This is done by <em>JavaScript</em>!');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+
 console.log('Script file is loaded.');
 
 // Single line comment
