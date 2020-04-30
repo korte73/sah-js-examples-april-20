558396e58d012fd555f44a2603b44eb171c95c34
Adam Gyulavari <adam.gyulavari@greenfoxacademy.com>
2020-04-30 18:55:25 +0200

    fourth part

--- scripts.js
+++ scripts.js
@@ -8,8 +8,16 @@ $('h2').toggleClass('blue');
 let name = 'Adam';
 let age = 34;
 
+age = age + 5;
+
 function myLittleFunction() {
-  $('main').append('<p>A p is appended in the main.</p>');
+  // condition: number of ps is smaller than 5
+  // <, >, <=, >=, ===, !==
+  if ($('p').length < 5) {
+    $('main').append('<p>We have a break until 18:06.</p>');
+  } else {
+    console.log('We reached the maximum paragraph number');
+  }
   $('p').css('border', '1px solid red');
   $('p').css('padding', '10px');
 }
@@ -21,6 +29,36 @@ $('#add').click(myLittleFunction);
 $('#remove').click(function () {
   $('p:last-child').remove();
 });
+
+// Fourth part:
+
+// if (condition that true or false) {
+  // condition is true
+// } else {
+  // condition is false
+// }
+
+let colors = ['orange', 'yellow', 'blue', 'lime', 'magenta'];
+
+colors.forEach(function (color) {
+  $('#box-container').append('<div class="box"></div>');
+  $('.box:last-child').css('background', color);
+});
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[0]);
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[1]);
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[2]);
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[3]);
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[4]);
 
 // Single line comment
 
